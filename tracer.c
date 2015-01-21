33adf2061b19e9bc0bd5db68a759dd489b9cf20e
Merlin GRIMALDI <mgrimald@e2r12p2.42.fr>
2015-01-21 17:12:41 +0100

    je t'ai eu espece de petit **L*P***

--- tracer.c
+++ tracer.c
@@ -2,42 +2,42 @@
 
 typedef struct	s_color
 {
-	int			red;
-	int			green;
-	int			blue;
+	unsigned int			red;
+	unsigned int			green;
+	unsigned int			blue;
 }				t_color;
 
 int		ft_col(t_trace t)
 {
 	t_color	c1;
 	t_color	c2;
-	int		color;
+	unsigned int		color;
 	int		progression;
 	int		tmp;
 	t_color	verif;
 
 	tmp = ((((t.x2 - t.x1) * (t.x2 - t.x1)) + ((t.y2 - t.y1) * (t.y2 - t.y1))));
-	progression = ((tmp * 100) / t.ref_prog) / 2;
+	progression = ((tmp * 255) / t.ref_prog) / 2;
 	progression = progression * 2;
-	if ((((tmp * 100) / t.ref_prog) % 10) >= 1)
+	if ((((tmp * 255) / t.ref_prog) % 2) >= 1)
 		progression = progression + 2;
-	if (progression > 100)
-		progression = 100;
-	c1.red = (t.c1 * (progression)) / (256 * 256 * 100);
-	c1.green = ((t.c1 * (progression)) / (256 * 100)) % 256;
-	c1.blue = ((t.c1 * (progression) / 100)) % (256);
-	c2.red = (t.c2 * (100 - progression)) / (256 * 256 * 100);
-	c2.green = ((t.c2 * (100 - progression)) / (256 * 100)) % 256;
-	c2.blue = (((t.c2 * (100 - progression)) / 100)) % (256);
+	if (progression > 255)
+		progression = 255;
+	c1.red = t.c1 / (256 * 256);
+	c1.green = (t.c1 / 256) % 256;
+	c1.blue = t.c1 % (256);
+	c2.red = t.c2 / (256 * 256);
+	c2.green = (t.c2 / 256) % 256;
+	c2.blue = (t.c2 % 256);
+	verif.red = ((c1.red * progression + c2.red * (255 - progression)) * 256 * 256);
+	verif.green = (c1.green * progression + c2.green * (255 - progression)) * 256;
+	verif.blue = (c1.blue * progression + c2.blue * (255 - progression));
 	printf("progression : %d\ttmp : %d\tt.ref_prog : %d\n", progression, tmp, t.ref_prog);
 	printf("t.c1 : %d\t\tt.c2 : %d\n", t.c1, t.c2);
 	printf("c1.red : %d\t\tc1.green : %d\t\tc1.blue : %d\n", c1.red, c1.green, c1.blue);
 	printf("c2.red : %d\t\tc2.green : %d\t\tc2.blue : %d\n", c2.red, c2.green, c2.blue);
-	verif.red = ((c1.red + c2.red) * 255 * 255);
-	verif.green = (c1.green + c2.green) * 255;
-	verif.blue = (c1.blue + c2.blue);
 	printf("verif.red : %d\t\tverif.green : %d\t\tverif.blue : %d\n\n", verif.red, verif.green, verif.blue);
-	color = verif.blue + verif.green + verif.red;
+	color = (verif.blue) / 255 + verif.green / 255 + verif.red / 255;
 	return (color);
 }
 
@@ -46,10 +46,7 @@ void	ft_trace_segm(t_fdf p1, t_fdf p2, t_env *env)
 	int			e;
 	t_trace		t;
 
-	t.ref_prog = (((t.dx * t.dx) + (t.dy * t.dy)) / 4);
 	//t.ref_prog = sqrt(((t.dx * t.dx) + (t.dy * t.dy)) / 4);
-	if (t.ref_prog == 0)
-		t.ref_prog = 1;
 	t.dx = (p2.aff_x - p1.aff_x) * 2;
 	t.dy = (p2.aff_y - p1.aff_y) * 2;
 	t.x1 = p1.aff_x;
@@ -58,6 +55,9 @@ void	ft_trace_segm(t_fdf p1, t_fdf p2, t_env *env)
 	t.y2 = p2.aff_y;
 	t.c1 = p1.color;
 	t.c2 = p2.color;
+	t.ref_prog = (((t.dx * t.dx) + (t.dy * t.dy)) / 4);
+	if (t.ref_prog == 0)
+		t.ref_prog = 1;
 	e = 0;
 	if (t.dx > 0 && t.dy > 0)
 		trace_1(t, e, env);
